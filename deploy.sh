b6b467e853eaca13791fb68d183e9b0920e2703e
Extasy <72500529+Extasy93@users.noreply.github.com>
2024-05-03 10:55:41 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@
 
 # Deploy via le proxy
 docker build -t cinema-projet .
-docker run -d --network=host --restart=always --name cinema-projet cinema-projet
+docker run -d --restart=always --name cinema-projet cinema-projet
 exit 0
